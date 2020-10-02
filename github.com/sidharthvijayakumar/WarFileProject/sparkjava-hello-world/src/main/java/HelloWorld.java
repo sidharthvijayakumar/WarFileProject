9db56ed43488df9011fa73b2dba601e8dbed421f
DELL <DELL@192.168.1.11>
2020-10-02 17:22:50 +0530

    5th

--- github.com/sidharthvijayakumar/WarFileProject/sparkjava-hello-world/src/main/java/HelloWorld.java
+++ github.com/sidharthvijayakumar/WarFileProject/sparkjava-hello-world/src/main/java/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "  Hello World  ");
+		get("/hello", (req, res) -> "  Hello World   ");
 		
 		
 	}
